c95761df32a19d2e4c951dce7fd0271deba264cc
camjaywilson <camjaywilson@gmail.com>
2018-11-17 14:24:13 -0700

    validation to register

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://cam:barkleigh9@ds161446.mlab.com:61446/dev-connector-cjw'
+    mongoURI: 'mongodb://cam:barkleigh9@ds161446.mlab.com:61446/dev-connector-cjw',
+    secretOrKey: 'Winston the dog'
 }
