34355a222039180c3541f44e8caf914417118af0
camjaywilson <camjaywilson@gmail.com>
2018-11-18 16:24:44 -0700

    Education and experience delete routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -200,5 +200,41 @@ router.post('/education', passport.authenticate('jwt', { session: false }), (req
         })
 })
 
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    delete experience from profile
+// @access  Private
+router.delete('/experience/:exp_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            const removeIndex = profile.experience
+                .map(item => item.id)
+                .indexOf(req.params.exp_id);
+
+            profile.experience.splice(removeIndex, 1);
+
+            profile.save().then(profile => res.json(profile)).catch(err => res.status(404).json(err))
+
+        })
+        .catch(err => res.status(404).json(err));
+})
+
+// @route   DELETE api/profile/education/:edu_id
+// @desc    delete education from profile
+// @access  Private
+router.delete('/education/:edu_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            const removeIndex = profile.education
+                .map(item => item.id)
+                .indexOf(req.params.edu_id);
+
+            profile.education.splice(removeIndex, 1);
+
+            profile.save().then(profile => res.json(profile)).catch(err => res.status(404).json(err))
+
+        })
+        .catch(err => res.status(404).json(err));
+})
+
 
 module.exports = router;
