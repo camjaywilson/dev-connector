f56afc0870a6d9397c12a8aa882b9327bf3605d1
camjaywilson <camjaywilson@gmail.com>
2018-11-20 20:46:07 -0700

    Post model added

--- models/Post.js
+++ models/Post.js
@@ -27,23 +27,25 @@ const PostSchema = new Schema({
         }
     ],
     comments: [
-        user: {
-            type: Schema.Types.ObjectId,
-            ref: 'users'
-        },
-        text: {
-            type: String,
-            required: true
-        },
-        name {
-            type: String,
-        },
-        avatar: {
-            type: String
-        },
-        date: {
-            type: Date,
-            default: Date.now
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            },
+            text: {
+                type: String,
+                required: true
+            },
+            name {
+                type: String,
+            },
+            avatar: {
+                type: String
+            },
+            date: {
+                type: Date,
+                default: Date.now
+            }
         }
     ],
     date: {
