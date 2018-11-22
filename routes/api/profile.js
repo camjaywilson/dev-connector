9561467256134259cbaedd0c55525534f7402cd3
camjaywilson <camjaywilson@gmail.com>
2018-11-21 17:22:35 -0700

    Post routes added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const mongooge = require('mongoose');
+const mongoose = require('mongoose');
 const passport = require('passport');
 
 const validateProfileInput = require('../../validation/profile');
